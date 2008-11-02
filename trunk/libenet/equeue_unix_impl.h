ba7c0d6a04c7251ba525fad5f854636f129cd115
eranif <eranif@3d9df747-5a54-0410-aaa4-ef901a837724>
2008-11-02 14:41:07 +0000

    - Fixed: queue timeout is now correctly calculated

--- trunk/libenet/equeue_unix_impl.h
+++ trunk/libenet/equeue_unix_impl.h
@@ -19,8 +19,8 @@ public:
 	eQueueImpl();
 	virtual ~eQueueImpl();
 
-	bool put(const T& item);
-	bool get(T& item, long timeout);
+	bool put ( const T& item );
+	bool get ( T& item, long timeout );
 };
 
 //----------------------------
@@ -30,13 +30,13 @@ template<class T>
 eQueueImpl<T>::eQueueImpl()
 {
 	pthread_mutexattr_t attr;
-	pthread_mutexattr_init(&attr);
-	pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_NORMAL);
-	pthread_mutex_init(&m_mutex, &attr);
-	
+	pthread_mutexattr_init ( &attr );
+	pthread_mutexattr_settype ( &attr, PTHREAD_MUTEX_NORMAL );
+	pthread_mutex_init ( &m_mutex, &attr );
+
 	pthread_condattr_t cattr;
-	pthread_condattr_init(&cattr);
-	pthread_cond_init(&m_cond, &cattr);
+	pthread_condattr_init ( &cattr );
+	pthread_cond_init ( &m_cond, &cattr );
 }
 
 template<class T>
@@ -45,27 +45,33 @@ eQueueImpl<T>::~eQueueImpl()
 }
 
 template<class T>
-bool eQueueImpl<T>::get(T& item, long timeout)
+bool eQueueImpl<T>::get ( T& item, long timeout )
 {
 	struct timespec	ts;
 	struct timeval	tv;
 
-	pthread_mutex_lock(&m_mutex);
+	pthread_mutex_lock ( &m_mutex );
 
-	while (m_queue.empty()) {
-		gettimeofday(&tv, NULL);
+	while ( m_queue.empty() ) {
+		gettimeofday ( &tv, NULL );
 
 		/* Convert from timeval to timespec */
 		ts.tv_sec  = tv.tv_sec;
 		ts.tv_nsec = tv.tv_usec * 1000;
 
 		// add our timeout
-		ts.tv_sec  += (timeout / 1000);
-		ts.tv_nsec += (timeout % 1000) * 1000000;
+		ts.tv_sec  += ( timeout / 1000 );
+		ts.tv_nsec += ( timeout % 1000 ) * 1000000;
+	
+		// nsec can not bre greater than 1000000000 (sec)
+		if ( ts.tv_nsec >= 1000000000 ) {
+			ts.tv_nsec -= 1000000000;
+			++ts.tv_sec;
+		}
 
-		int rc = pthread_cond_timedwait(&m_cond, &m_mutex, &ts);
-		if (rc == ETIMEDOUT) {
-			pthread_mutex_unlock(&m_mutex);
+		int rc = pthread_cond_timedwait ( &m_cond, &m_mutex, &ts );
+		if ( rc == ETIMEDOUT ) {
+			pthread_mutex_unlock ( &m_mutex );
 			return false;
 		}
 	}
@@ -74,21 +80,21 @@ bool eQueueImpl<T>::get(T& item, long timeout)
 	item = m_queue.front();
 	m_queue.pop_front();
 
-	pthread_mutex_unlock(&m_mutex);
+	pthread_mutex_unlock ( &m_mutex );
 
 	return true;
 }
 
 template<class T>
-bool eQueueImpl<T>::put(const T& item)
+bool eQueueImpl<T>::put ( const T& item )
 {
-	pthread_mutex_lock(&m_mutex);
+	pthread_mutex_lock ( &m_mutex );
 	bool was_empty = m_queue.empty();
-	m_queue.push_back(item);
-	if (was_empty) {
-		pthread_cond_signal(&m_cond);
+	m_queue.push_back ( item );
+	if ( was_empty ) {
+		pthread_cond_signal ( &m_cond );
 	}
-	pthread_mutex_unlock(&m_mutex);
+	pthread_mutex_unlock ( &m_mutex );
 	return true;
 }
 
--- trunk/libenet_test/test_equeue.cpp
+++ trunk/libenet_test/test_equeue.cpp
@@ -35,7 +35,7 @@ public:
 		while ( true ) {
 			std::string item;
 
-			if ( !g_queue2.get ( item, 1000 ) ) {
+			if ( !g_queue2.get ( item, 500 ) ) {
 				printf ( "queue timeout\n" );
 			} else {
 				printf ( "got item from queue\n" );
@@ -44,55 +44,55 @@ public:
 	}
 };
 
-TEST(TestGet)
-{
-	eQueue<std::string> myq;
-	std::string output;
-
-	// put one element on the queue
-	CHECK(myq.put("eran"));
-	CHECK(myq.get(output, -1));
-	CHECK(myq.get(output, 10) == false);
-	CHECK(myq.get(output, 10) == false);
-}
-
-TEST(TestPut)
-{
-	eQueue<std::string> myq;
-	std::string output;
-
-	// put one element on the queue
-	CHECK(myq.put("eran"));
-}
-
-TEST(ConcurrentTest)
-{
-	ConsumerThread thr[10];
-
-	for(size_t i=0; i<10; i++){
-		thr[i].run();
-	}
-
-	for(size_t i=0; i<100; i++){
-		g_queue.put("eran");
-	}
-
-	// wait for thread to terminate itself
-	for(size_t i=0; i<10; i++){
-		thr[i].wait(-1);
-	}
-	CHECK(g_counter==100);
-}
-
-//TEST ( BlockingTest )
+//TEST(TestGet)
 //{
-//	for ( size_t i=0; i<100; i++ ) {
-//		g_queue2.put ( "eran" );
-//	}
-//	
-//	WorkerThread thr;
-//	thr.run();
-//	
-//	sleep(10000);
+//	eQueue<std::string> myq;
+//	std::string output;
+//
+//	// put one element on the queue
+//	CHECK(myq.put("eran"));
+//	CHECK(myq.get(output, -1));
+//	CHECK(myq.get(output, 10) == false);
+//	CHECK(myq.get(output, 10) == false);
 //}
 //
+//TEST(TestPut)
+//{
+//	eQueue<std::string> myq;
+//	std::string output;
+//
+//	// put one element on the queue
+//	CHECK(myq.put("eran"));
+//}
+//
+//TEST(ConcurrentTest)
+//{
+//	ConsumerThread thr[10];
+//
+//	for(size_t i=0; i<10; i++){
+//		thr[i].run();
+//	}
+//
+//	for(size_t i=0; i<100; i++){
+//		g_queue.put("eran");
+//	}
+//
+//	// wait for thread to terminate itself
+//	for(size_t i=0; i<10; i++){
+//		thr[i].wait(-1);
+//	}
+//	CHECK(g_counter==100);
+//}
+
+TEST ( BlockingTest )
+{
+	for ( size_t i=0; i<100; i++ ) {
+		g_queue2.put ( "eran" );
+	}
+	
+	WorkerThread thr;
+	thr.run();
+	
+	sleep(10000);
+}
+
